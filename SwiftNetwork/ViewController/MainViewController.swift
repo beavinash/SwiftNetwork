e8f46cca94e4fe5d005f6016c730e95ca0e7995c
Avinash Reddy <beavinash15@gmail.com>
2019-04-14 12:41:29 -0700

    project architecture completed

---
+++ SwiftNetwork/ViewController/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  SwiftNetwork
+//
+//  Created by Avinash Reddy on 4/14/19.
+//  Copyright © 2019 Avinash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
